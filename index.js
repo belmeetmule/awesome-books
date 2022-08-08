b0a44101fd58f1d56f855e5d27ab7d6040c31790
mule <belmeetmule@gmail.com>
2022-08-09 02:03:41 +0300

    add local storage script

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// array of the book objects
 let book = [];
 
 const addBook = () => {
@@ -13,63 +13,60 @@ const addBook = () => {
     //display new book
     if(bookTitle.value && bookAuthor.value){
         const display = document.querySelector('.book-display');
-        console.log(display);
-    const div = document.createElement('div');
-    div.className = 'book-list';
+        const div = document.createElement('div');
+        div.className = 'book-list';
 
-    display.appendChild(div);
+        display.appendChild(div);
 
-    const title = document.createElement('p');
-    title.textContent=newBook.title;
-    title.className='class-title';
+        const title = document.createElement('p');
+        title.textContent=newBook.title;
+        title.className='class-title';
 
-    div.appendChild(title);
+        div.appendChild(title);
 
-    const author = document.createElement('p');
-    author.textContent=newBook.author;
-    author.className='class-title';
+        const author = document.createElement('p');
+        author.textContent=newBook.author;
+        author.className='class-title';
 
-    div.appendChild(author);
+        div.appendChild(author);
 
-    const remove = document.createElement('button');
-    remove.textContent='Remove';
-    remove.className ='remove';
+        const remove = document.createElement('button');
+        remove.textContent='Remove';
+        remove.className ='remove';
 
-    div.appendChild(remove);
-    
-    remove.addEventListener('click', () => {
-        div.style.display = 'none';
-        for(let i=0; i< book.length; i++){
-            if(book[i].title === title.textContent){
-                book.splice(i,1);
+        div.appendChild(remove);
+        
+        remove.addEventListener('click', () => {
+            div.style.display = 'none';
+            for(let i=0; i< book.length; i++){
+                if(book[i].title === title.textContent){
+                    book.splice(i,1);
+                }
             }
+        }); 
+        bookTitle.value='';
+        bookAuthor.value='';     
         }
-    }); 
-    bookTitle.value='';
-    bookAuthor.value='';     
-    }
 }
 
 //add button
 const addButton = document.querySelector('.btn');
 addButton.addEventListener('click', addBook);
 
-/*
-function addToUI(book){
-   
+const saveInput = () =>{
+    const bookContent = {
+        title: document.querySelector('.title').value,
+        author: document.querySelector('.author').value
+    };
 
+    window.localStorage.setItem('bookContent', JSON.stringify(bookContent));
+};
+
+window.onload=()=>{
+    const bookContent= JSON.parse(window.localStorage.getItem('bookContent'));
+    console.log(bookContent);
+    if(bookContent){
+        document.querySelector('.title').value = bookContent.title;
+        document.querySelector('.author').value = bookContent.author;
+    }
 }
-
-function addBook(){
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    book.title=title;
-    book.author=author;
-    console.log(book);
-}
-
-const form = document.getElementById('form');
-console.log('i am here');
-form.addEventListener('submit', addBook);
-
-*/
